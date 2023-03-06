02577a9aa2858ba08872a025adb1060f22b45fec
unknown <dreicleal@hotmail.com>
2023-03-06 17:48:44 -0300

    retrieve update delete view

--- users/views.py
+++ users/views.py
@@ -2,7 +2,7 @@ from .models import User
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from .serializers import UserSerializer
-# from .permissions import 
+from .permissions import IsAccountOwnerOrAdmin
 
 
 class UserView(ListCreateAPIView):
@@ -11,4 +11,8 @@ class UserView(ListCreateAPIView):
 
 
 class UserDetailView(RetrieveUpdateDestroyAPIView):
-    ...
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAccountOwnerOrAdmin]
+
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
