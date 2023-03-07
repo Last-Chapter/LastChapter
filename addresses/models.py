bee68d12cdc41185d0cb6ee21ed4ed61a4cdda15
Henrique Jesus <henrique.jesus128@gmail.com>
2023-03-07 17:22:31 -0300

    fix: add code_zip model address

--- addresses/models.py
+++ addresses/models.py
@@ -6,6 +6,7 @@ class Address(models.Model):
     number = models.IntegerField()
     district = models.CharField(max_length=50)
     city = models.CharField(max_length=50)
+    code_zip = models.CharField(max_length=10)
 
     def __repr__(self) -> str:
         return f"<Address ({self.id}) - {self.street}, {self.number}>"
