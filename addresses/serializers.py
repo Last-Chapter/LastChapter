140f1a6f6e72e1291544b71f4b521bb102fe7565
Henrique Jesus <henrique.jesus128@gmail.com>
2023-03-07 12:00:16 -0300

    feat: serializer address

---
+++ addresses/serializers.py
@@ -0,0 +1,29 @@
+from rest_framework.serializers import ModelSerializer
+from rest_framework.validators import UniqueValidator
+from .models import Address
+
+
+class AddressSerializer(ModelSerializer):
+    class Meta:
+        model = Address
+        fields = [
+            "id",
+            "street",
+            "number",
+            "district",
+            "city",
+            "users",
+        ]
+        extra_kwargs = {"id": {"read_only": True}}
+
+    def create(self, validated_data: dict) -> Address:
+        return Address.objects.create(**validated_data)
+
+
+def update(self, instance: Address, validated_data: dict) -> Address:
+    for key, value in validated_data.items():
+        setattr(instance, key, value)
+
+    instance.save()
+
+    return instance
