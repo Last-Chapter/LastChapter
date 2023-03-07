15ea96b454057fab9e81aadbcc1ad357753faed8
Henrique Jesus <henrique.jesus128@gmail.com>
2023-03-07 11:59:51 -0300

    feat: viwes address CRUD

--- addresses/views.py
+++ addresses/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from .models import Address
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
-# Create your views here.
+from .serializers import AddressSerializer
+from ..users.permissions import IsAccountOwnerOrAdmin
+
+
+class AddressView(ListCreateAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAccountOwnerOrAdmin]
+    queryset = Address.objects.all()
+    serializer_class = AddressSerializer
+
+
+class AdressDetailView(RetrieveUpdateDestroyAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAccountOwnerOrAdmin]
+
+    queryset = Address.objects.all()
+    serializer_class = AddressSerializer
