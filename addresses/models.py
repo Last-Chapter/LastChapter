dea02f9110184f47ca21dc4db71ea57e8f7badf1
Henrique Jesus <henrique.jesus128@gmail.com>
2023-03-07 11:42:47 -0300

    fix: remove cep

--- addresses/models.py
+++ addresses/models.py
@@ -6,7 +6,6 @@ class Address(models.Model):
     number = models.IntegerField()
     district = models.CharField(max_length=50)
     city = models.CharField(max_length=50)
-    cep = models.CharField(max_length=10)
 
     def __repr__(self) -> str:
         return f"<Address ({self.id}) - {self.street}, {self.number}>"
