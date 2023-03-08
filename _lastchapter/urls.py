bc17d97072193eea8a6675f70c6309d1c3a5d762
Angelica Assini <assini@gmail.com>
2023-03-08 08:37:25 -0300

    add feat books

--- _lastchapter/urls.py
+++ _lastchapter/urls.py
@@ -21,6 +21,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("users.urls")),
     path("api/", include("addresses.urls")),
+    path("api/", include("books.urls")),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/docs/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
 ]
