3f5088d0fcc77afad51d4e13407a857492972dbc
Henrique Jesus <henrique.jesus128@gmail.com>
2023-03-08 12:13:49 -0300

    fix: views books post

--- books/views.py
+++ books/views.py
@@ -2,23 +2,21 @@ from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework_simplejwt.authentication import JWTAuthentication
-from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly, IsAdminUser
-# from .permissions import IsBookOwnerOrAdmin
+from permissions.isAccountOwnerOrAdmin import IsAccountOwnerOrAdmin
+
 
 class BookView(generics.ListCreateAPIView):
     authentication_classes = [JWTAuthentication]
-    permission_classes = [IsAuthenticatedOrReadOnly, IsAdminUser]
-    
+    permission_classes = [IsAccountOwnerOrAdmin]
+
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
-    def perform_create(self, serializer):
-        serializer.save(user_followers=self.request.user)
 
 class BookDetailView(generics.RetrieveAPIView):
-    authentication_classes = [JWTAuthentication]
-    permission_classes = [IsAuthenticated]
+    # authentication_classes = [JWTAuthentication]
+    # permission_classes = [IsAuthenticated]
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    
+
     lookup_url_kwarg = "book_id"
