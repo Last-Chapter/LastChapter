03e9b5613aaa7c3ea7685630fdf168c9153430e3
Henrique Jesus <henrique.jesus128@gmail.com>
2023-03-10 10:34:28 -0300

    fix add copy/borrowing/return/<uuid:copy_id>/

--- copies/urls.py
+++ copies/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
-from .views import CopyView, CopyDetailView, CopyListView, CopyBorrowingView
+from .views import (
+    CopyView,
+    CopyDetailView,
+    CopyListView,
+    CopyBorrowingView,
+    ReturnBorrowingView,
+)
 
 urlpatterns = [
     path("copies/<uuid:book_id>/", CopyView.as_view()),
     path("copies/", CopyListView.as_view()),
     path("copy/<uuid:copy_id>/", CopyDetailView.as_view()),
     path("copy/borrowing/<uuid:copy_id>/", CopyBorrowingView.as_view()),
+    path("copy/borrowing/return/<uuid:copy_id>/", ReturnBorrowingView.as_view()),
 ]
