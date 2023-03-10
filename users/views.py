2321463a97dde28aae00a5a3670c04fe36118b34
unknown <dreicleal@hotmail.com>
2023-03-10 08:56:34 -0300

    feat: borrow history

--- users/views.py
+++ users/views.py
@@ -1,8 +1,9 @@
 from .models import User
 from rest_framework_simplejwt.authentication import JWTAuthentication
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
-from .serializers import UserSerializer
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView, ListAPIView
+from .serializers import UserSerializer, BorrowHistorySerializer
 from permissions.isAccountOwnerOrAdmin import IsAccountOwnerOrAdmin
+from copies.models import Borrowing
 
 
 class UserView(ListCreateAPIView):
@@ -16,3 +17,14 @@ class UserDetailView(RetrieveUpdateDestroyAPIView):
 
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+class UserBorrowHistoryView(ListAPIView):
+    authentication_classes = [JWTAuthentication]
+    serializer_class = BorrowHistorySerializer
+
+    def get_queryset(self):
+        if self.request.user.is_superuser:
+            return Borrowing.objects.all()
+
+        return Borrowing.objects.filter(user=self.request.user)
